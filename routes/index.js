ed4124651e18989ba04094a5347e15d302cb7768
Rishabh Mittal <rishabh19038@yahoo.in>
2018-02-20 12:08:13 +0530

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -107,7 +107,7 @@ io.sockets.on('connection', function(socket){
 });
 
 server.listen(5001, function(){
-    console.log('Socket server running succesfully on port 3000');
+    console.log('Socket server running succesfully on port 5001');
 });
 
 module.exports = router;
